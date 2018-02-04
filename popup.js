fa0e92b6868208017a99eaaa33d6e5ab52db5957
leo-jm <32468691+leo-jm@users.noreply.github.com>
2018-02-04 09:39:46 -0800

    Comments

--- popup.js
+++ popup.js
@@ -1,28 +1,29 @@
+//Test Function
 function myFunction() {
 	var x = document.getElementById("address").value;
 	if (x.length != 0) {
 		document.getElementById("demo").innerHTML = x;
 		}	
 	}
-function checkload(callback){
+//Loads the page, initial function
+function checkload(){
 	getpagevals(load)
-	/*document.getElementById('mainpage').addEventListener('load',getpagevals(load))*/
-	checkbuttons()
+	//document.getElementById('mainpage').addEventListener('load',getpagevals(load))
 }
+//Decides what buttons to check for
 function checkbuttons(){
 	page1class = document.getElementById("page1").className
 	page2class = document.getElementById("page2").className
 	console.log(page2class)
 	if (page2class == 'open'){
-		document.getElementById("demo2").innerHTML = 'test'
 		checkcancelpage2()
 	}else{
 		if(page1class == 'open'){
-		document.getElementById("demo").innerHTML = 'test'
 		checkbuttonpress()
 		}
 	}
 }
+//Function for going to page 2
 function page2(){
 	document.getElementById("page1").className='closed'
 	document.getElementById("page2").className='open'
@@ -35,10 +36,12 @@ function page2(){
 			var page1class = 'closed'
 			var page2class = 'open'
 		}
+		//Saving page and address data to storage
 		chrome.storage.sync.set({'page1':page1val,'page2':page2val,'address':x, 'page1class':page1class,'page2class':page2class});
 		getpagevals(load)
 
 }
+//Retrieve page and address data from storage
 function getpagevals(callback){
 	var pagevals = [];
 	chrome.storage.sync.get(['page1','page2','address','page1class','page2class'], function(items){
@@ -48,6 +51,7 @@ function getpagevals(callback){
 		}
 	});
 }
+//Loads in the page based on saved data, called in the callback for function getpagevals()
 function load(val){
 	var page1val = 'base'
 	var page2val = 'base'
@@ -63,17 +67,22 @@ function load(val){
 	page1.className = page1class
 	page2.className = page2class
 	document.getElementById("addresstext").innerHTML = address;
+	checkbuttons()
 }
+// Checks for the enter button to be pressed on page 1
 function checkbuttonpress(){
 	document.getElementById("page1").className='open'
 	document.getElementById("page2").className='closed'
 	document.getElementById("enterbutton").addEventListener('click',page2)
 }
+//Checks for the cancel button to be pressed on page 2
 function checkcancelpage2(){
 	document.getElementById("cancelpage2").addEventListener('click',resetpages)
 }
+//Sets the page data and address datat back to default
 function resetpages(){
 	chrome.storage.sync.set({'page1':'block','page2':'none','address':'', 'page1class':'open','page2class':'closed'});
 	getpagevals(load)
 }
+//Initial Function called
 checkload()
